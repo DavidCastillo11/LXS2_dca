395eb798755b8fe26ebb3d78c56f790dc7c8f74e
David Castillo A <danca1105@hotmail.com>
2016-11-05 11:48:16 -0600

    agrego al sistema las ultimas modificaciones del script (puestos habilitados/interfaces activas)

--- src/reporte.sh
+++ src/reporte.sh
@@ -4,3 +4,16 @@ REPORTE=reporte-`date +%Y%m%d`.log
 
 echo "REPORTE GENERADO POR $USER:" > $REPORTE
 echo " " >> $REPORTE
+echo " " >> $REPORTE
+
+echo "#######################################" >> $REPORTE
+echo "#Interfaces disponibles en el servidor#" >> $REPORTE
+echo "#######################################" >> $REPORTE
+echo " " >> $REPORTE
+
+ifconfig >> $REPORTE
+
+echo "####################################" >> $REPORTE
+echo "#Puertos habilitados en el servidor#" >> $REPORTE
+echo "####################################" >> $REPORTE
+netstat -lautp | grep LISTEN >> $REPORTE
